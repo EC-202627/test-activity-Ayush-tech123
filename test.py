52da6456b9352287d766d27e9e86a5e89865f7c6
Ayush-tech123 <ayush.sharma@avantika.edu.in>
2026-03-26 07:16:31 +0000

    Executed

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+'''
 X ml of water today. Determine if the client followed the doctor's advice or not.
 Input Format
 
@@ -5,3 +6,12 @@ The first line contains a single integer
 T — the number of test cases. Then the test cases follow.
 The first and only line of each test case contains one integer 
 X — the amount of water client drank today.
+'''
+
+T = int(input())
+for i in range(T):
+    X = int(input())
+    if X>=2000:
+        print("Yes")
+    else:
+        print("No")     
